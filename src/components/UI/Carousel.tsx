713c579a3aaf3da211c1b8bd308ae2aac02ded48
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-01-21 18:00:50 +0300

    Настройка корусели с жанрами

--- src/components/UI/Carousel.tsx
+++ src/components/UI/Carousel.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react';
 import {useTypedSelector} from "../../hooks/useTypedSelector";
 import {useAction} from "../../hooks/useAction";
+import AliceCarousel from "react-alice-carousel";
+import 'react-alice-carousel/lib/alice-carousel.css';
+import './Carousel.css'
 
 const Carousel: React.FC = () => {
     const {genre, loading, error} = useTypedSelector(state => state.movie)
@@ -8,18 +11,26 @@ const Carousel: React.FC = () => {
 
     useEffect(() => {
         fetchGenre()
-        console.log(genre)
     }, [])
-    if(loading){
+
+    const items = genre.map(g => <div className='carousel-itme'><img width={407} alt={'img'} src={g.img}/><h2>{g.name}</h2></div>)
+    const responsive = {
+        0: {items: 1},
+        568: {items: 2},
+        1024: {items: 3},
+    };
+
+    if (loading) {
         return <h1>Идет загрузка</h1>
     }
-    if(error){
+    if (error) {
         return <h1>{error}</h1>
     }
     return (
-        <div>
-            {genre.map(g => <div key={g.id}>{g.name}</div>)}
+        <div className='carousel'>
+            <AliceCarousel items={items} responsive={responsive}/>
         </div>
+
     );
 };
 
