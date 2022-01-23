24dce87877f9b5a3651a8923133350067b9c67a9
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-01-23 15:31:04 +0300

    Создание компаненты Load.tsx

---
+++ src/components/UI/Load/Load.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import load from './load.svg'
+
+
+const Load = () => {
+    return (
+        <div>
+            <img width={200} alt='loader' src={load}/>
+        </div>);
+};
+
+export default Load;
