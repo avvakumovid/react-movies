7901856ae2ff1a70b350eb91b4ca59509e55bcda
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-01-27 21:22:30 +0300

    Настройка стилей Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Button from "../UI/Button/Button";
-import {headerStyle} from "./HeaderStyle";
+import {button, headerStyle, input} from './HeaderStyle';
 
 const Header: React.FC = () => {
     return (
         <div className="container" style={headerStyle}>
-            <input className='searchInput' type='search'/>
-            <Button style={{}} callback={() => {}}  >Login</Button>
+            <input style={input} type='search'/>
+            <button style={button} >Login</button>
         </div>
     );
 };
--- src/components/Header/HeaderStyle.ts
+++ src/components/Header/HeaderStyle.ts
@@ -6,3 +6,23 @@ export const headerStyle: CSSProperties = {
     justifyContent: 'space-between',
     height: 'auto'
 }
+
+export const input: CSSProperties = {
+    backgroundColor: '#29313a',
+    width: '90%',
+    margin: 10,
+    borderRadius: 5,
+    border: '1px solid #91c8f6',
+    color: '#91c8f6',
+    padding: 5
+}
+
+export const button: CSSProperties = {
+    fontSize: 20,
+    padding: 5,
+    margin: 10,
+    borderRadius: 5,
+    backgroundColor: '#232a31',
+    border: '1px solid #91c8f6',
+    color: '#91c8f6',
+}
