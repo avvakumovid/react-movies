d9644b9f68e64c2b48513f6e822b4f6f74e7a3be
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-01-28 13:43:48 +0300

    Настройка бэкграунда

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {CSSProperties, useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {useAction} from '../../hooks/useAction';
 import {block} from '../MovieList/MovieStyle';
@@ -12,7 +12,7 @@ import {useTypedSelector} from '../../hooks/useTypedSelector';
 
 const Detail: React.FC = () => {
     let {id} = useParams()
-    let {fetchMovieById,resetMovie} = useAction()
+    let {fetchMovieById, resetMovie} = useAction()
     useEffect(() => {
         fetchMovieById(id)
         return () => {
@@ -22,11 +22,30 @@ const Detail: React.FC = () => {
     let {loading, error, movie} = useTypedSelector(state => state.movie)
     let [color, setColor] = useState('')
     let [src, setSrc] = useState('')
+    const backgroundImage: CSSProperties = {
+        backgroundImage: `url(\'https://image.tmdb.org/t/p/original/${movie?.backdrop_path}\')`,
+        backgroundSize: 'cover',
+        display: 'block',
+        filter: 'blur(10px)',
+        opacity: '0.7',
+        // webkitFilter: 'blur(5px)',
+        height: '800px',
+        left: 0,
+        position: 'fixed',
+        right: 0,
+        zIndex: 1,
+    }
+    const content: CSSProperties = {
+        position: 'relative',
+        zIndex: 2,
+        display: 'flex',
+        padding: 40
+    }
     useEffect(() => {
-            if(movie){
-                setColor(getRaitingColor(movie.vote_average))
-                setSrc(`https://image.tmdb.org/t/p/w500/${movie.poster_path}`)
-            }
+        if (movie) {
+            setColor(getRaitingColor(movie.vote_average))
+            setSrc(`https://image.tmdb.org/t/p/w500/${movie.poster_path}`)
+        }
     }, [movie])
 
     if (error) {
@@ -39,19 +58,22 @@ const Detail: React.FC = () => {
         <div style={block}>
             {(loading || !movie)
                 ? <Load/>
-                : <div style={style}>
-                    <div style={img}>
-                        <img width={300} alt="poster path" src={src}/>
-                    </div>
-                    <div style={info}>
-
-                        <h1>{movie?.title}</h1>
-                        <Trailer style={padding} witdh="560" height="315" moviePath={movie.treilerId}
-                                 title="YouTube video player"/>
-                        <ReactStars count={10} value={movie.vote_average} size={50} edit={false} half={true} color2={color}/>
-                        <h3 style={{...padding, color}} color={color}>Raiting: {movie.vote_average}</h3>
-                        <h3 style={padding}>Relese: {movie.release_date}</h3>
-                        <div style={padding}><strong>Overview:</strong> {movie.overview}</div>
+                : <div >
+                    <div style={backgroundImage}></div>
+                    <div style={content}>
+                        <div style={img}>
+                            <img width={300} alt="poster path" src={src}/>
+                        </div>
+                        <div style={info}>
+                            <h1>{movie?.title}</h1>
+                            <Trailer style={padding} witdh="560" height="315" moviePath={movie.treilerId}
+                                     title="YouTube video player"/>
+                            <ReactStars count={10} value={movie.vote_average} size={50} edit={false} half={true}
+                                        color2={color}/>
+                            <h3 style={{...padding, color}} color={color}>Raiting: {movie.vote_average}</h3>
+                            <h3 style={padding}>Relese: {movie.release_date}</h3>
+                            <div style={padding}><strong>Overview:</strong> {movie.overview}</div>
+                        </div>
                     </div>
                 </div>}
         </div>
